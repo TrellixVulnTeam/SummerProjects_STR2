f1d6a076c7f545da2182089287548937643e118d
Fengbo Zhao <fireheart54@gmail.com>
2017-08-17 11:48:27 -0500

    GUI test

---
+++ Projects/Test/Test/GUItest.py
@@ -0,0 +1,18 @@
+import Tkinter as tk
+import tkMessageBox as mb
+
+
+window = tk.Tk()
+
+answer = mb.askokcancel("Question", "Do you want to open this file?")
+
+
+
+
+
+window.mainloop()
+
+
+
+
+
